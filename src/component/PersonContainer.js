fb98d8056986f14d57b23080213b575f5307c93b
Will Jones <willjones2010@gmail.com>
2019-09-19 00:09:52 -0400

    findCharacterFrequency function working

--- src/component/PersonContainer.js
+++ src/component/PersonContainer.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import Person from './Person'
 
 class People extends Component {
-    state = {
-        people: []
+    constructor(){
+        super()
+        this.state = {
+            people: []
+        }
     }
 
     findCharacterFrequency(people) {
+        
         let listOfEmails = []
         let output =[]
+        let counter = {}
 
         people.map((person) => {
             if (person.email_address) {
@@ -17,15 +22,15 @@ class People extends Component {
         })
         
         listOfEmails.map((email) => {
-            const counter = {}
+            
             for(let x = 0 ; x < email.length; x++) {
-                const character = email.charAt(x);
+                let character = email.charAt(x);
                 counter[character] = (isNaN(counter[character]) ? 1 : counter[character] + 1)
             }
             output.push(counter);
         });
+        console.log(counter)
     }
-  
 
     render() {
         let people = this.props.allPeople.data ? this.props.allPeople.data : null
@@ -34,10 +39,17 @@ class People extends Component {
                 
                 <h3>List of People</h3>
 
+                <div>
+                    <button onClick={() => this.findCharacterFrequency(people)}>Find Character Frequency</button>
+                </div>
+
                 {people ? people.map(obj => {
                     return <div id={obj.id}> <Person key={obj.id} info={obj} /> </div>
                 }) : null}
+
+           
             </div>
+            
         )
     }
 
